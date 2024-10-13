b08cf4b9cbbe18ae5f3f79dd55a80e3c857ef4a7
GodConker <danielcastro.f50@gmail.com>
2024-10-13 16:01:23 -0700

    Modificaciones de las Dimensiones de los Frames
    Añadí las dimensiones estándar que estaremos manejando para las Interfaces Gráficas. Con posibles futuras modificaciones.

--- ProyectoPanaderia/ProyectoPanaderia/src/main/java/Presentacion/FrmManejoEmpleados.java
+++ ProyectoPanaderia/ProyectoPanaderia/src/main/java/Presentacion/FrmManejoEmpleados.java
@@ -27,16 +27,17 @@ public class FrmManejoEmpleados extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(564, 435));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 564, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 435, Short.MAX_VALUE)
         );
 
         pack();
